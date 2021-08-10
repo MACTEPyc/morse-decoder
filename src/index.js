32bb8efd38b015dd7f3413e1557e394be5e7747f
MACTEPyc <MACTEPyc@gmail.com>
2021-08-10 14:40:30 +0500

    feat: add function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const CODE_TO_MORSE = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+}
+
 function decode(expr) {
-    // write your solution here
+    let decodeMorse = '';
+    for (let i = 0; i < expr.length / 10; i++) {
+        let morseStr = '';
+        subStr = expr.substr(i * 10, 10);
+        if (subStr === '**********') {
+            decodeMorse += ' ';
+        } else {
+            for (let j = 0; j < subStr.length / 2; j++) {
+                morseStr += CODE_TO_MORSE[subStr.substr(j * 2, 2)];
+            }
+            decodeMorse += MORSE_TABLE[morseStr];  
+        }
+    }
+    return decodeMorse;
 }
 
 module.exports = {
